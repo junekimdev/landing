4bf79901b7a2337fca88437905df3caf26d80888
June Kim <godlyjune@gmail.com>
2020-01-03 04:31:29 +0900

    Enable build id assignment and more
    # Details
    - Enable build id assignment
    - Add env.PUBLIC_URL for SSR
    - Disable 'powered by' header

--- next.config.js
+++ next.config.js
@@ -1,2 +1,17 @@
 const withSass = require('@zeit/next-sass');
-module.exports = withSass();
+module.exports = withSass({
+  // @see https://nextjs.org/docs/api-reference/next.config.js/environment-variables
+  env: {
+    PUBLIC_URL: null,
+  },
+  generateBuildId: async () => {
+    // @see https://github.com/zeit/next.js/wiki/Deployment
+    // When process.env.BUILD_ID is undefined, fall back to the default
+    if (process.env.BUILD_ID) {
+      return process.env.BUILD_ID;
+    }
+
+    return null;
+  },
+  poweredByHeader: false,
+});
