d57417b838a77d6087d22be569abc1e838a7fe70
June Kim <godlyjune@gmail.com>
2022-01-18 14:39:00 +0900

    Upgrade deps

--- components/footer/footerViewEngine.tsx
+++ components/footer/footerViewEngine.tsx
@@ -6,7 +6,7 @@ const view = () => {
       <span>Powered by</span>
       <img src="/assets/junekim_192x192.png" alt="Developer logo" className={styles.logo} />
       <span>
-        <a href="https://github.com/JuneKimDev" rel="noopener">
+        <a href="https://github.com/JuneKimDev" rel="noopener noreferrer">
           June Kim
         </a>
       </span>
